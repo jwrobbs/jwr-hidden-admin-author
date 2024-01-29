7b5959fc8ecb6d577966a9d4bf0daab1f80b9a04
Josh Robbs <josh@joshrobbs.com>
2024-01-29 02:52:21 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Main file for Hidden Admin Author plugin.
+ *
+ * @author Josh Robbs <josh@joshrobbs.com>
+ * @since 2024-01-29
+ * @package Hidden_Admin_Author
+ */
+
+namespace HiddenAdminAuthor;
+
+defined( 'ABSPATH' ) || die( 'No script kiddies please!' );
+
+/*
+	[] add_hidden_admin_author_settings
+	[] is_author_admin
+	[] get_substitute_author
+	[] replace_admin_author
+*/
